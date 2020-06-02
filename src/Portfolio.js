e67018304d22b732842e8e5ac6aaf1f61f0ee224
peacebonez <kmpariso12@gmail.com>
2020-06-02 14:17:09 -0400

    Just beginning!

---
+++ src/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/Nav";
+import "./App.css";
+
+function Portfolio() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="nav-container">
+      <h1>Nav</h1>
+    </div>
+  );
+}
