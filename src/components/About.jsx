ef93b43797fdfed92e3a8edf12f0002361062c58
peacebonez <kmpariso12@gmail.com>
2020-06-04 13:14:24 -0400

    implementing mobile images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { FaJs, FaHtml5, FaCss3Alt, FaReact, FaNpm } from "react-icons/fa";
 
+let hue = 1;
+let light = 98;
+let deg = 90;
+let flagHue = true;
+let flagLight = true;
+let flagDeg = true;
+
+//triggers the interval
+
+let colorIncrement = () => setInterval(colorFunc, 200);
+
 export default function About() {
   return (
     <div className="about-container">
@@ -11,7 +22,7 @@ export default function About() {
       </h3>
       <div className="icons-div">
         <div>
-          <FaJs className="tech-icon" />
+          <FaJs className="tech-icon" id="jsIcon" />
           <FaReact className="tech-icon" />
           <FaNpm className="tech-icon" />
         </div>
@@ -20,6 +31,57 @@ export default function About() {
           <FaCss3Alt className="tech-icon" />
         </div>
       </div>
+      <div id="resume-container">
+        <a href="">
+          <button id="resume-btn">Resume</button>
+        </a>
+      </div>
     </div>
   );
 }
+
+function colorFunc() {
+  // Hue alteration
+  if (hue === 360 || hue === 0) flagHue = !flagHue;
+
+  if (flagHue) {
+    hue += 0.5;
+    // console.log('Hue up:', hue);
+  } else {
+    hue -= 0.5;
+    // console.log('Hue down:' ,hue);
+  }
+
+  // Light alteration
+  if (light < 49 || light > 99) flagLight = !flagLight;
+
+  if (flagLight) {
+    light += 0.1;
+    // console.log("Light up:", Math.round(light));
+  } else {
+    light -= 0.1;
+    // console.log("Light down:", Math.round(light));
+  }
+
+  //Gradient degree alteration
+  if (deg === 360 || deg === 0) flagDeg = !flagDeg;
+
+  if (flagDeg) {
+    deg++;
+    // console.log("Deg up:", Math.round(deg));
+  } else {
+    deg--;
+    // console.log("Deg down:", Math.round(deg));
+  }
+  // bodyMain.style.backgroundImage = `linear-gradient(${deg}deg, hsl(${hue}, 50%, ${light}%), hsl(${hue + 30}, 50%, ${light + 20}%))`;
+
+  document.body.style.backgroundImage = `linear-gradient(${deg}deg, hsl(${hue}, 100%, ${light}%), hsl(${
+    hue + 30
+  }, 100%, ${light + 20}%))`;
+
+  // bodyMain.style.background = `hsl(${hue}, 100%, ${light}%)`; // Neon
+  // bodyMain.style.background = `hsl(${hue}, 50%, ${light}%)`; // Balance between the 2
+  // console.log(bodyMain.style.backgroundImage)
+}
+
+colorIncrement();
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,10 @@ import { FaGithubSquare, FaLinkedin, FaEnvelope } from "react-icons/fa";
 export default function Nav() {
   return (
     <div className="nav-container">
-      <h1>Kevin M. Pariso</h1>
-      <h2>Web Developer</h2>
+      <div>
+        <h1>Kevin M. Pariso</h1>
+        <h2>Web Developer</h2>
+      </div>
       <ul className="nav-list">
         <a>
           <li id="git-square">
