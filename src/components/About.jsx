734ce4adcb1bf5cd3ef293d37ba2fc15c6911e71
peacebonez <kmpariso12@gmail.com>
2020-06-15 21:11:20 -0400

    About Me tweaks

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,30 +5,28 @@ export default function About() {
   return (
     <div className="about-container">
       <h3>
-        What up! I'm Kevin and I'm a nascent web developer living in Brooklyn
-        NY. For the past year or so I've hit learning Front End Development on
-        my own pretty hard. I've accomplished tasks initially thought to be
-        impossible, and I'm looking forward to doing it again in the future, but
-        with a team at my side. Learning has always been a passion of mine, but
-        at some point <span id="learn">the learning must become doing</span>{" "}
-        which brings us this portfolio website. <br></br>Please do have a look
-        around, and if you feel inclined, there's plenty of opportunities to
-        reach out!
+        What up! I'm Kevin and I'm a web developer living in Brooklyn NY. For
+        the past year or so I've hit learning Front End Development on my own
+        pretty hard. I've accomplished tasks initially thought to be impossible,
+        and I'm looking forward to doing it again in the future, but with a team
+        at my side. Learning has always been a passion of mine, but at some
+        point <span id="learn">the learning must become doing</span> which
+        brings us this portfolio website. <br></br>Please do have a look around,
+        and if you feel inclined, there's plenty of opportunities to reach out!
       </h3>
       <ul>
         <h1>Core Technologies</h1>
         <li>Javascript</li>
         <li>React (Hooks & Class Components)</li>
         <li>HTML</li>
-        <li>CSS (Responsive Design, Flex-box)</li>
+        <li>CSS (SCSS, Responsive, Flex-box)</li>
       </ul>
       <ul>
         <h1>Addtional Experience With...</h1>
         <li>Bootstrap</li>
         <li>JQuery</li>
-        <li>SQL</li>
+        <li>SQL & MongoDB</li>
         <li>Python</li>
-        <li>C</li>
       </ul>
       <div className="icons-div">
         <div>
