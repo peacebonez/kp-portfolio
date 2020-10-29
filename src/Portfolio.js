ded7816170673e7a266e2dead7ffb4af5d8775b2
peacebonez <kmpariso12@gmail.com>
2020-10-29 12:05:33 -0400

    attempt usage of google analytics

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
+
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Email from "./components/Email";
 import "./App.css";
-
 import gradient from "./gradient";
 
 function Portfolio() {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Portfolio from "./Portfolio";
+import { Router } from "react-router";
+
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+import TRACKING_ID from "./keys";
+
+ReactGA.initialize(TRACKING_ID);
+console.log(ReactGA);
+
+const history = createBrowserHistory();
+history.listen((location) => {
+  console.log("location:", location);
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history={history}>
     <Portfolio />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );
