dfece832a32519224f115afc3ed84d567b23fdd8
peacebonez <kmpariso12@gmail.com>
2020-12-08 19:43:25 -0500

    add worldchat

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,13 @@ import { FaChevronDown } from "react-icons/fa";
 
 const projects = [
   {
+    name: "World Chat",
+    link: "https://world-chat-mern.herokuapp.com/",
+    gitLink: "https://github.com/peacebonez/world-chat",
+    largeImg: "https://i.imgur.com/1NwifmG.png",
+    smallImg: "https://i.imgur.com/1NwifmG.png",
+  },
+  {
     name: "Facetune Fails Blog",
     link: "https://radiant-plateau-54256.herokuapp.com/",
     gitLink: "https://github.com/peacebonez/facetune-fails",
@@ -45,34 +52,6 @@ const projects = [
     largeImg: "https://i.imgur.com/FyGCClS.png",
     smallImg: "https://i.imgur.com/odp6Ga8.jpg",
   },
-  // {
-  //   name: "Vaporwave Memory Game",
-  //   link: "https://memory-card-game-with-hooks.netlify.app/",
-  //   gitLink: "https://github.com/peacebonez/memory-app-with-hooks",
-  //   largeImg: "https://i.imgur.com/FlWcltw.png",
-  //   smallImg: "https://i.imgur.com/DQ72IyF.jpg",
-  // },
-  // {
-  //   name: "Self Marketing Twitter Bot",
-  //   link: "https://twitter.com/kpizo3",
-  //   gitLink: "https://github.com/peacebonez/twitter-bot-v2",
-  //   largeImg: "https://media.giphy.com/media/fSFbBPmr4HPRwW0VMo/giphy.gif",
-  //   smallImg: "https://media.giphy.com/media/fSFbBPmr4HPRwW0VMo/giphy.gif",
-  // },
-  // {
-  //   name: "Drum Machine",
-  //   link: "https://fcc-drum-machine-react.netlify.app/",
-  //   gitLink: "https://github.com/peacebonez/FCC-drum-machine",
-  //   largeImg: "https://i.imgur.com/UfpjhHg.png",
-  //   smallImg: "https://i.imgur.com/EtYd8jx.jpg",
-  // },
-  // {
-  //   name: "Virtual Library",
-  //   link: "https://virtual-library.netlify.app/",
-  //   gitLink: "https://github.com/peacebonez/library-app",
-  //   largeImg: "https://i.imgur.com/btqDo6Q.png",
-  //   smallImg: "https://i.imgur.com/H2hW2l8.jpg",
-  // },
 ];
 let id = 0;
 export default function Projects() {
